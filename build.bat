24340ed36afc2a1d639d7465d394c5cb36fe6149
Alexandre Perché <66827361+vegasword@users.noreply.github.com>
2024-05-01 20:14:10 +0200

    Enhanced compilation time by updating build script and updated gitignore

--- build.bat
+++ build.bat
@@ -2,8 +2,13 @@
 where cl.exe > nul 2>&1
 if %errorlevel% neq 0 (call setup.bat)
 
+ctime -begin compile_time.txt
+
 rem DEBUG
-cl src/main.c /Fegltf2custom.exe /Zi /nologo /Iinclude
+cl src/main.c /Iinclude /Yccgltf.h /Od /MTd /Zi /Fegltf2custom.exe /nologo /GR /EHa /Oi /fp:fast /Fm /FC
 
 rem RELEASE
-rem cl src/main.c /DNDEBUG /O2 /Fegltf2custom.exe /nologo /Iinclude
+rem cl src/main.c /Iinclude /Yccgltf.h /Ox /MT /DNDEBUG /Fegltf2custom.exe /nologo /GR /EHa /Oi /fp:fast /Fm /FC
+
+ctime -end compile_time.txt
+del compile_time.txt > NUL
