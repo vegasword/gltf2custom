317853be5eef999972a4745ff17a70d6cc393eb9
Alexandre Perché <66827361+vegasword@users.noreply.github.com>
2025-05-14 21:32:16 +0200

    Simplified code and updated scripts

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 set DEBUG=1
 set IFLAGS=/Iinclude /Yccgltf.h
-set CFLAGS=/Fegltf2custom /nologo /GR /EHa /Oi /fp:fast /FC /link /INCREMENTAL:NO /opt:ref
+set CFLAGS=/Fegltf2custom /nologo /W4 /D_CRT_SECURE_NO_WARNINGS /GR /EHa /Oi /fp:fast /FC /link /INCREMENTAL:NO /opt:ref
 
 rem INSTALL THE LATEST VERSION OF BUILD TOOLS AND ITS SETUP BATCH SCRIPT
 rem @ https://github.com/Data-Oriented-House/PortableBuildTools/releases
@@ -11,10 +11,10 @@ if %errorlevel% neq 0 (call setup.bat)
 
 ctime -begin compile_time.txt
 
-if %DEBUG% == "1" (
-  cl src/main.c %IFLAGS% /Od /MTd /Zi /Fm %CFLAGS%
+if "%DEBUG%" == "1" (
+  cl src/main.c %IFLAGS% /Od /MTd /Zi /Fm /DDEBUG=1 %CFLAGS%
 ) else (
-  cl src/main.c %IFLAGS% /Ox /MT /DNDEBUG %CFLAGS%
+ cl src/main.c %IFLAGS% /Ox /MT /DNDEBUG %CFLAGS%
 )
 
 ctime -end compile_time.txt
